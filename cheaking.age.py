be65d65360430af7a54bc7c0414b690b3656255d
Alithebest-coder <aurumprint@proton.me>
2024-12-29 13:23:20 +1000

    Add files via upload

---
+++ cheaking.age.py
@@ -0,0 +1,9 @@
+even_age=int(input("Enter a even age number for the code please:   "))
+odd_age=int(input("Enter a odd age number for the code please:   "))
+
+if odd_age in range(odd_age>even_age):
+    print("odd age is bigger then even age")
+    
+else:
+    print("even age is bigger then odd age")
+  
